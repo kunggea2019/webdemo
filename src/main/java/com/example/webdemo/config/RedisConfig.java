1758cb6d9b5b8893e2add092975c4aad6e46921c
liukun <liukun@tiens.com>
2021-10-22 14:58:08 +0800

    增加redis配置,Spring生成redis配置bean

---
+++ src/main/java/com/example/webdemo/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.webdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+// 增加redis配置,Spring生成redis配置bean
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
